0b1b41d0b8f7dfbe5fa38e124540219605b90a60
Arima <smurfers968@gmail.com>
2025-05-27 14:02:21 +0800

    Add solution for exercise1.2

---
+++ part1/courseinfo/1.1/exercise1.2/src/App.jsx
@@ -0,0 +1,49 @@
+const Header = (props) => {
+  console.log(props.course);
+  return <h1>{props.course}</h1>
+}
+
+const Part = (props) => {
+  console.log(props.part, props.exercise);
+
+  return (
+    <p>
+      {props.part} {props.exercise}
+    </p>
+  )
+}
+
+const Content = (props) => { 
+
+  // This console.log is for debugging purposes to check the values of props
+  return (
+    <div>
+      <Part part = 'Fundamentals of React' exercise = {10}/>
+      <Part part = 'Using props to pass data' exercise = {7}/>
+      <Part part = 'State of a component' exercise = {14}/>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+    </p>
+  )
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  
+  return (
+    <div>
+      <Header course={course} />
+      <Content />
+      <Total exercises1={10} exercises2={7} exercises3={14} />
+    </div>
+  )
+}
+
+export default App
