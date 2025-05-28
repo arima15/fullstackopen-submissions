74eedc85cd0bf34f6dc954676aac41499280a282
Arima <smurfers968@gmail.com>
2025-05-28 17:16:59 +0800

    Add: exercise 1.4 submission for fullstackOpen

--- part1/courseinfo/1.1/exercise1.2/src/App.jsx
+++ part1/courseinfo/1.1/exercise1.2/src/App.jsx
@@ -35,26 +35,27 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   return (
     <div>
       <Header course={course} />
-      <Content part={part1.name} exercise={part1.exercises} />
-      <Content part={part2.name} exercise={part2.exercises} />
-      <Content part={part3.name} exercise={part3.exercises} />
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+      <Content part={parts[0].name} exercise={parts[0].exercises} />
+      <Content part={parts[1].name} exercise={parts[1].exercises} />
+      <Content part={parts[2].name} exercise={parts[2].exercises} />
+      <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises} />
     </div>
   )
 }
