19a17676955c516d1a3b5cad5c8be73343122329
Arima <smurfers968@gmail.com>
2025-05-28 17:20:25 +0800

    Add: exercise 1.5 submission for fullstackOpen

--- part1/courseinfo/1.1/exercise1.2/src/App.jsx
+++ part1/courseinfo/1.1/exercise1.2/src/App.jsx
@@ -34,28 +34,30 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
-      <Header course={course} />
-      <Content part={parts[0].name} exercise={parts[0].exercises} />
-      <Content part={parts[1].name} exercise={parts[1].exercises} />
-      <Content part={parts[2].name} exercise={parts[2].exercises} />
-      <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises} />
+      <Header course={course.name} />
+      <Content part={course.parts[0].name} exercise={course.parts[0].exercises} />
+      <Content part={course.parts[1].name} exercise={course.parts[1].exercises} />
+      <Content part={course.parts[2].name} exercise={course.parts[2].exercises} />
+      <Total exercises1={course.parts[0].exercises} exercises2={course.parts[1].exercises} exercises3={course.parts[2].exercises} />
     </div>
   )
 }
