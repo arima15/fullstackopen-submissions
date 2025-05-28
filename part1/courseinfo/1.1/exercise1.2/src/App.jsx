adfc1189289588a852ae1584b0b8067303dcb775
Arima <smurfers968@gmail.com>
2025-05-28 17:11:26 +0800

    Add: exercise 1.3 submission for fullstackOpen

--- part1/courseinfo/1.1/exercise1.2/src/App.jsx
+++ part1/courseinfo/1.1/exercise1.2/src/App.jsx
@@ -4,7 +4,8 @@ const Header = (props) => {
 }
 
 const Part = (props) => {
-  console.log(props.part, props.exercise);
+  console.log(props.part, props.exercise); // This will log the values of name and exercise
+
 
   return (
     <p>
@@ -14,13 +15,11 @@ const Part = (props) => {
 }
 
 const Content = (props) => { 
-
+   
   // This console.log is for debugging purposes to check the values of props
   return (
     <div>
-      <Part part = 'Fundamentals of React' exercise = {10}/>
-      <Part part = 'Using props to pass data' exercise = {7}/>
-      <Part part = 'State of a component' exercise = {14}/>
+      <Part part = {props.part} exercise = {props.exercise}/>
     </div>
   )
 }
@@ -36,12 +35,26 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
   return (
     <div>
       <Header course={course} />
-      <Content />
-      <Total exercises1={10} exercises2={7} exercises3={14} />
+      <Content part={part1.name} exercise={part1.exercises} />
+      <Content part={part2.name} exercise={part2.exercises} />
+      <Content part={part3.name} exercise={part3.exercises} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
     </div>
   )
 }
